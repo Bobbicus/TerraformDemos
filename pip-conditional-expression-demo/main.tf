c6fceb1ef9d401c9c2b56cdce7a5af20abd1a05f
Robert Larkin <robert.larkin@devopsgroup.com>
2021-09-27 17:05:11 +0100

    test conflict

--- pip-conditional-expression-demo/main.tf
+++ pip-conditional-expression-demo/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = join("", [var.resourcegroup1,var.environment, "-rg"])
+  name     = join("", [var.resourcegroup,var.environment, "-rg"])
   location = var.location
 }
 
